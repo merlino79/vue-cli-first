d29837180748759f91e440cc17a127aed3c48118
Merlino79 <alessandro.ciancio@hotmail.com>
2021-05-19 16:09:31 +0200

    errore

---
+++ src/components/Title.vue
@@ -0,0 +1,19 @@
+<template>
+  <title>
+    <h1>
+      il mio primo Node - Vue
+    </h1>
+  </title>
+</template>
+
+<script>
+export default {
+
+  name: 'Title'
+
+}
+</script>
+
+<style>
+
+</style>
